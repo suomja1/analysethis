36b072016a124ea93cb7ea38fccbc5d56b781588
Janne <suom.jann@gmail.com>
2017-01-19 21:53:19 +0200

    Tyhjät luokat

---
+++ src/main/java/dna/analysethis/analysethis.java
@@ -0,0 +1,7 @@
+package dna.analysethis;
+
+public class analysethis {
+    public static void main(String[] args) {
+        // TODO
+    }
+}
---
+++ src/test/java/dna/analysethis/analysethisTest.java
@@ -0,0 +1,11 @@
+package dna.analysethis;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class analysethisTest {    
+    @Test
+    public void testNoTest() {
+        assertTrue(true);
+    }
+}
